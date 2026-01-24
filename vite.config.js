975f30c8243db3081724ed6cab7263fc67e7da19
Danny <179722984+DanielProano@users.noreply.github.com>
2026-01-23 23:57:15 -0500

    Fix to UI library?

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   plugins: [react()],
